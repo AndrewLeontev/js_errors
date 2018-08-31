1abb1104a98b9dd89edddacb1c138e0cc012eecd
Andrey Leontev <leontev.andrey.s@gmail.com>
2018-08-31 14:28:08 +1000

    Add solved code for lessons 2 and 4

---
+++ 02_trees/Tree.js
@@ -0,0 +1,67 @@
+class Tree {
+    constructor(key, meta, parent) {
+      this.parent = parent;
+      this.key = key;
+      this.meta = meta;
+      this.children = new Map();
+    }
+  
+    getKey() {
+      return this.key;
+    }
+  
+    getMeta() {
+      return this.meta;
+    }
+  
+    addChild(key, meta) {
+      const child = new Tree(key, meta, this);
+      this.children.set(key, child);
+  
+      return child;
+    }
+  
+    getChild(key) {
+      return this.children.get(key);
+    }
+  
+    // BEGIN (write your solution here)
+    hasChildren() {
+  
+      return this.children.size > 0;
+    }
+  
+    hasChild(key) {
+      return this.children.has(key);
+    }
+  
+    getParent() {
+      return this.parent
+    }
+  
+    getDeepChild(keys) {
+      if (keys.length === 0) {
+        return null;
+      }
+      const [key, ...rest] = keys;
+      const node = this.getChild(key);
+      if (node === undefined) {
+        return null;
+      } else if (rest.length === 0) {
+        return node;
+      }
+      return node.getDeepChild(rest);
+    }
+  
+    removeChild(key) {
+      return this.children.delete(key);
+    }
+  
+    getChildren() {
+      return [...this.children.values()];
+    }
+    // END
+  }
+  
+  export default Tree;
+  
